6777e70700f323935299bb3e1948535d8f47339d
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-02-24 21:54:58 +0530

    More tests

--- test1.c
+++ test1.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void norawmode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 void rawmode(){
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
-    tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(norawmode);
+    
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main(){
     rawmode();
     char c;
-    while(read(STDIN_FILENO,&c,1)==1 && c!='q');
-    printf("Public static\n");
+    while(read(STDIN_FILENO,&c,1)==1 && c!='q'){
+        if (iscntrl(c)) {
+            printf("%d\n",c);
+        }
+        else {
+            printf("%d ('%c')\n",c,c);
+        }
+    }
 }
