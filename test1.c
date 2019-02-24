2ded50d20c1eaca84186c8dd3c4195bcc9f1c57e
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-02-24 21:24:12 +0530

    More tests

--- test1.c
+++ test1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+
+void rawmode(){
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &- ~(ECHO);
+    tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw);
+}
 int main(){
+    rawmode();
     char c;
     while(read(STDIN_FILENO,&c,1)==1 && c!='q');
     printf("Public static\n");
