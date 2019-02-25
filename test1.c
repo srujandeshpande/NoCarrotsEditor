fa5e7d1b1f54266a15a84c17b3e01be6e0d515b6
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-02-25 07:08:44 +0530

    More tests-Terminal IO

--- test1.c
+++ test1.c
@@ -3,31 +3,79 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <errno.h>
 
+#define CTRL_KEY(k) ((k)&0x1F)
+
+/* data*/
 struct termios orig_termios;
 
-void norawmode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+/*for terminal*/
+void die(const char*s){
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+	perror(s);
+	exit(1);
+}
+void noRawMode(){
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)==-1)
+		die("tcsetattr");
 }
 
-void rawmode(){
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(norawmode);
+void rawMode(){
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+		die("tcgetattr");
+    atexit(noRawMode);
     
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN]=0;
+	raw.c_cc[VTIME]=1;
     
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) 
+		die("tcsetattr");
 }
+
+char readKey() {
+	int n;
+	char c;
+	while (n = read(STDIN_FILENO, &c, 1)!=1) {
+		if(n ==-1 && errno != EAGAIN)
+			die("read");
+	}
+	return c;
+}
+
+
+/*for output*/
+void refreshScreen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+	
+
+/*for input*/
+void processKeys() {
+	char c = readKey();
+	
+	switch (c) {
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+			exit(0);
+			break;
+	}
+}
+
+/*init*/
 int main(){
-    rawmode();
-    char c;
-    while(read(STDIN_FILENO,&c,1)==1 && c!='q'){
-        if (iscntrl(c)) {
-            printf("%d\n",c);
-        }
-        else {
-            printf("%d ('%c')\n",c,c);
-        }
-    }
+    rawMode();
+	while(1) {
+		refreshScreen();
+		processKeys();
+	}
+	return 0;
 }
