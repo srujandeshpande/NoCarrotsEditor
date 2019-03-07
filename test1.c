a00414cd75942732a41b859449fce08813f680f9
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-03-07 07:18:31 +0530

    step49

--- test1.c
+++ test1.c
@@ -185,10 +185,18 @@ void refreshScreen() {
 
 void moveCursor(int key) {
 	switch(key) {
-		case ARROW_LEFT: E.cx--; break;
-		case ARROW_RIGHT: E.cx++; break;
-		case ARROW_UP: E.cy--; break;
-		case ARROW_DOWN: E.cy++; break;
+		case ARROW_LEFT:
+			if(E.cx!=0) E.cx--; 
+			break;
+		case ARROW_RIGHT:
+			if(E.cx!= E.screencols -1) E.cx++; 
+			break;
+		case ARROW_UP:
+			if(E.cy!=0) E.cy--;
+			break;
+		case ARROW_DOWN:
+			if(E.cy!= E.screenrows -1) E.cy++;
+			break;
 	}
 }
 
