08d9c641a29c16927bfd4854efe4b7700145dc91
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-03-07 06:53:53 +0530

    step45

--- test1.c
+++ test1.c
@@ -160,6 +160,16 @@ void refreshScreen() {
 
 
 /*for input*/
+
+void moveCursor(char key) {
+	switch(key) {
+		case 'a': E.cx--; break;
+		case 'd': E.cx++; break;
+		case 'w': E.cy--; break;
+		case 's': E.cy++; break;
+	}
+}
+
 void processKeys() {
 	char c = readKey();
 	
@@ -169,13 +179,19 @@ void processKeys() {
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
+		case 'w':
+		case 's':
+		case 'a':
+		case 'd':
+			moveCursor(c);
+			break;
 	}
 }
 
 /*init*/
 void initEditor() {
 	E.cx = 0;
-	R.cy = 0;
+	E.cy = 0;
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
