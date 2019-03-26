d19f20ba567f14ca35a49baf7e03e6497a9f2842
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-03-26 22:00:58 +0530

    Latest step

--- test1.c
+++ test1.c
@@ -10,7 +10,7 @@
 #define VERSION "0.1.47"
 #define CTRL_KEY(k) ((k)&0x1F)
 enum editorKey {
-	ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN};
+	ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN,  PAGE_UP, PAGE_DOWN};
 
 /* data*/
 struct editorConfig {
@@ -67,11 +67,22 @@ int readKey() {
 		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 		
 		if (seq[0] =='[') {
-			switch (seq[1]) {
-				case 'A': return ARROW_UP;
-				case 'B': return ARROW_DOWN;
-				case 'C': return ARROW_RIGHT;
-				case 'D': return ARROW_LEFT;
+			if (seq[1] >= '0' && seq[1] <= '9') {
+				if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+				if(seq[2] == '~') {
+					switch(seq[1]) {
+						case 5: return PAGE_UP;
+						case 6: return PAGE_DOWN;
+					}
+				}
+			}
+			else {
+				switch (seq[1]) {
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+				}
 			}
 		}
 		
@@ -208,6 +219,12 @@ void processKeys() {
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+		case PAGE_UP: 
+		case PAGE_DOWN:{
+			int times = E.screenrows;
+			while(times--) moveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			}
 			break;
 		case ARROW_UP:
 		case ARROW_DOWN:
