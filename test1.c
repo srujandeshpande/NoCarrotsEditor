f740e2350416632b5a32e7d4c7c70ade8a07729d
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-04-01 21:23:08 +0530

    Step104

--- test1.c
+++ test1.c
@@ -21,7 +21,7 @@
 #define CTRL_KEY(k) ((k)&0x1F)
 
 enum editorKey {
-	ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN, DEL_KEY, HOME_KEY, END_KEY,
+	BACKSPACE = 127, ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN, DEL_KEY, HOME_KEY, END_KEY,
 	  PAGE_UP, PAGE_DOWN};
 
 /* data*/
@@ -214,6 +214,24 @@ void editorAppendRow(char *s, size_t len) {
 	E.numrows++;
 }
 
+void rowInsertChar(erow *row, int at, int c) {
+	if(at <0 || at> row->size) at = row->size;
+	row->chars = realloc(row->chars, row->size +2);
+	memmove(&row->chars[at + 1], &row->chars[at], row->size -at +1);
+	row->size++;
+	row->chars[at] = c;
+	editorUpdateRow(row);
+}
+
+/*editor operations*/
+void insertChar(int c) {
+	if(E.cy == E.numrows) {
+		editorAppendRow("",0);
+	}
+	rowInsertChar(&E.row[E.cy], E.cx, c);
+	E.cx++;
+}
+	
 
 /*file io*/
 void editorOpen(char *filename) {
@@ -409,6 +427,10 @@ void processKeys() {
 	int c = readKey();
 	
 	switch (c) {
+		case '\r':
+			//TODO
+			break;
+			
 		case CTRL_KEY('q'):
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
@@ -420,6 +442,11 @@ void processKeys() {
 		case END_KEY:
 			if(E.cy < E.numrows)
 				E.cx = E.row[E.cy].size;
+			break;
+		case BACKSPACE:
+		case CTRL_KEY('h'):
+		case DEL_KEY:
+			//TODO
 			break;
 		case PAGE_UP: 
 		case PAGE_DOWN:{
@@ -439,6 +466,14 @@ void processKeys() {
 		case ARROW_LEFT:
 		case ARROW_RIGHT:
 			moveCursor(c);
+			break;
+		
+		case CTRL_KEY('l'):
+		case '\x1b':
+			break;
+			
+		default:
+			insertChar(c);
 			break;
 	}
 }
