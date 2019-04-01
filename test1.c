4ea1693a14dc78448af78c5bd57c5deee0494d75
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-04-01 18:27:36 +0530

    Step76

--- test1.c
+++ test1.c
@@ -287,9 +287,17 @@ void moveCursor(int key) {
 	switch(key) {
 		case ARROW_LEFT:
 			if(E.cx!=0) E.cx--; 
+			else if (E.cy >0) {
+				E.cy--;
+				E.cx = E.row[E.cy].size;
+			}
 			break;
 		case ARROW_RIGHT:
 			if (row && E.cx < row->size) E.cx++; 
+			else if(row && E.cx == row->size) {
+				E.cy++;
+				E.cx = 0;
+			}
 			break;
 		case ARROW_UP:
 			if(E.cy!=0) E.cy--;
@@ -298,6 +306,12 @@ void moveCursor(int key) {
 			if(E.cy < E.numrows) E.cy++;
 			break;
 	}
+	
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  	int rowlen = row ? row->size : 0;
+	if (E.cx > rowlen) {
+		E.cx = rowlen;
+	}	
 }
 
 void processKeys() {
