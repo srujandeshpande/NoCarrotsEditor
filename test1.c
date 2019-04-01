6063ab0984018cef35d54d44ffda49486f7e4beb
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-04-01 18:37:31 +0530

    Step81

--- test1.c
+++ test1.c
@@ -24,7 +24,9 @@ enum editorKey {
 /* data*/
 typedef struct erow {
 	int size;
+	int rsize;
 	char *chars;
+	char *render;
 } erow;
 
 struct editorConfig {
@@ -156,6 +158,19 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*row operations*/
+void editorUpdateRow(erow *row) {
+	free(row->render);
+	row->render = malloc(row->size + 1);
+	int j;
+	int idx = 0;
+	for (j = 0; j < row->size; j++) {
+		row->render[idx++] = row->chars[j];
+	}
+	row->render[idx] = '\0';
+	row->rsize = idx;
+}
+
+
 void editorAppendRow(char *s, size_t len) {
 	E.row = realloc(E.row, sizeof(erow) * (E.numrows +1));
 	
@@ -164,6 +179,11 @@ void editorAppendRow(char *s, size_t len) {
 	E.row[at].chars = malloc(len+1);
 	memcpy(E.row[at].chars, s, len);
 	E.row[at].chars[len] = '\0';
+	
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+	editorUpdateRow(&E.row[at]);
+	
 	E.numrows++;
 }
 
